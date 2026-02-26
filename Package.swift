702bd89d8157e5044027799bc715ccbef65331ef
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-02-26 10:27:02 +0100

    Flatten test structure and migrate to Apple Swift Testing

--- Package.swift
+++ Package.swift
@@ -45,7 +45,13 @@ let package = Package(
                 .rfc5321,
                 .rfc5890
             ]
-        )
+        ),
+        .testTarget(
+            name: "Domain Standard Tests",
+            dependencies: [
+                "Domain Standard",
+            ]
+        ),
     ],
     swiftLanguageModes: [.v6]
 )
@@ -58,6 +64,7 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
         .enableUpcomingFeature("ExistentialAny"),
         .enableUpcomingFeature("InternalImportsByDefault"),
         .enableUpcomingFeature("MemberImportVisibility"),
+        .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
         .enableExperimentalFeature("Lifetimes"),
         .enableExperimentalFeature("SuppressedAssociatedTypes"),
         .enableExperimentalFeature("SuppressedAssociatedTypesWithDefaults"),
